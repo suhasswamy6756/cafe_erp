157f9010606cc8acc1d7ed8f903bb4c6efdf1d2d
suhasswamy6756 <suhasswamy6756@gmail.com>
2026-03-18 12:46:31 +0530

    feat: add Redis configuration and enable caching for improved performance

--- src/main/java/com/cafe/erp/common/config/RedisConfig.java
+++ src/main/java/com/cafe/erp/common/config/RedisConfig.java
@@ -1,24 +1,24 @@
-package com.cafe.erp.common.config;
-
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-
-@Configuration
-@EnableCaching
-public class RedisConfig {
-
-    @Bean
-    public RedisCacheConfiguration cacheConfiguration() {
-        return RedisCacheConfiguration.defaultCacheConfig()
-                .serializeValuesWith(
-                        RedisSerializationContext.SerializationPair.fromSerializer(
-                                new GenericJackson2JsonRedisSerializer()
-                        )
-                )
-                .disableCachingNullValues();
-    }
-}
+//package com.cafe.erp.common.config;
+//
+//import org.springframework.cache.annotation.EnableCaching;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.cache.RedisCacheConfiguration;
+//import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+//import org.springframework.data.redis.serializer.RedisSerializationContext;
+//
+//@Configuration
+//@EnableCaching
+//public class RedisConfig {
+//
+//    @Bean
+//    public RedisCacheConfiguration cacheConfiguration() {
+//        return RedisCacheConfiguration.defaultCacheConfig()
+//                .serializeValuesWith(
+//                        RedisSerializationContext.SerializationPair.fromSerializer(
+//                                new GenericJackson2JsonRedisSerializer()
+//                        )
+//                )
+//                .disableCachingNullValues();
+//    }
+//}
